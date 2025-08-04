b9bfee54d6432a426cdcf4dc8180077c901bcaf1
luiscarlos.alvarenga24@gmail.com <luiscarlos.alvarenga24@gmail.com>
2025-08-03 21:20:09 -0600

    feat : Esta sera la estructura base del programa, en la que declaramos variables, arreglos de procutos y precios.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //ESTRUCTURA BASE DEL PROGRAMA
+        Scanner lc = new Scanner(System.in);
+        //ARREGLOS DE LOS PRODUCTOS Y PRECIOS
+        String[] productos = {"Camisa", "Pantalón", "Zapatos", "pulseras", "Mochila"};
+        double[] precios = {20.0, 25.0, 40.0, 15.0, 30.0};
+
+        int[] carrito = new int[20]; //MAXIMA CAPACIDAD DE ALMACENAMIENTO DE PRODUCTOS
+        int total_prod = 0;
+        int opcion;
+
+    }
+}
